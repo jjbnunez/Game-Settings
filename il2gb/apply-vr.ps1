fbd54f18307533631c5931884157c13511d68a65
jjbnunez <jjbnunez97@gmail.com>
2022-04-28 12:41:13 -0400

    Save settings for IL-2 GB

---
+++ il2gb/apply-vr.ps1
@@ -0,0 +1,3 @@
+$GameData = "D:\Steam Games\steamapps\common\IL-2 Sturmovik Battle of Stalingrad\data"
+
+Copy-Item ".\vr.cfg" "$GameData\startup.cfg"
---
+++ il2gb/apply.ps1
@@ -0,0 +1,3 @@
+$GameData = "D:\Steam Games\steamapps\common\IL-2 Sturmovik Battle of Stalingrad\data"
+
+Copy-Item ".\screen.cfg" "$GameData\startup.cfg"
