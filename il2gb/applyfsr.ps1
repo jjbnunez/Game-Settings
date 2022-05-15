dc77b7dfc3bfbe8d658ccf6b5067e018dd5019f5
leaflizer <grrrovyle@gmail.com>
2022-05-15 14:25:40 -0400

    Complete applyfsr script

--- il2gb/applyfsr.ps1
+++ il2gb/applyfsr.ps1
@@ -1,6 +1,9 @@
 . .\common.ps1
 
-$FsrUri = "https://github.com/fholger/openvr_fsr/releases/download/fsr_v2.1.1/openvr_fsr_v2.1.1.zip"
+$FsrVersion = "2.1.1"
+$FsrDirName = "openvr_fsr_v$FsrVersion"
+$FsrArchive = "$FsrDirName.zip"
+$FsrUri = "https://github.com/fholger/openvr_fsr/releases/download/fsr_v$FsrVersion/$FsrArchive"
 
 if (-not (Test-Path "$GameDir\openvr_api.dll.bak")) {
     Copy-Item -Force "$GameDir\openvr_api.dll" "$GameDir\openvr_api.dll.bak"
@@ -11,5 +14,13 @@ Do {
     $TempDir = Join-Path $env:TEMP $TempDirName
 }
 Until (-not (Test-Path "$TempDir"))
+New-Item -Path $env:TEMP -Name $TempDirName -ItemType Directory | Out-Null
 
-Invoke-WebRequest -UseBasicParsing $FsrUri -Out
+Invoke-WebRequest -UseBasicParsing -Uri $FsrUri -OutFile "$TempDir\$FsrArchive"
+
+Expand-Archive -LiteralPath "$TempDir\$FsrArchive" -DestinationPath "$TempDir\$FsrDirName"
+
+Copy-Item -Force "$TempDir\$FsrDirName\openvr_api.dll" -Destination "$GameDir"
+Copy-Item -Force ".\openvr_mod.cfg" -Destination "$GameDir"
+
+Remove-Item -Recurse "$TempDir\"
