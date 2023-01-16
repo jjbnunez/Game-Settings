9e4a52b1f8c71ef5ab91e66fcfb94fc3bdeabda5
JJ Nunez <jjbnunez97@gmail.com>
2023-01-16 00:56:17 -0500

    Change install directory

--- il2gb/common.ps1
+++ il2gb/common.ps1
@@ -1,4 +1,4 @@
-$InstallDir = "D:\Steam Games\steamapps\common\IL-2 Sturmovik Battle of Stalingrad"
+$InstallDir = "C:\Program Files (x86)\Steam\steamapps\common\IL-2 Sturmovik Battle of Stalingrad"
 $DataDir = Join-Path $InstallDir "\data"
 $BinDir = Join-Path $InstallDir "\bin"
 $GameDir = Join-Path $BinDir "\game"
