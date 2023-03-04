a41105ff6161669d38a217c6b55207489d90db71
JJ Nunez <jjbnunez97@gmail.com>
2023-03-04 11:42:59 -0500

    Print filepaths to screen on common.ps1 import

--- il2gb/common.ps1
+++ il2gb/common.ps1
@@ -2,3 +2,8 @@ $InstallDir = "C:\Program Files (x86)\Steam\steamapps\common\IL-2 Sturmovik Batt
 $DataDir = Join-Path $InstallDir "\data"
 $BinDir = Join-Path $InstallDir "\bin"
 $GameDir = Join-Path $BinDir "\game"
+
+Write-Host "InstallDir is '$InstallDir'"
+Write-Host "DataDir is '$DataDir'"
+Write-Host "BinDir is '$BinDir'"
+Write-Host "GameDir is '$GameDir'"
