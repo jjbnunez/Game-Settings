c36394845a4d70a580b70b36c39e4fc989a66bdf
JJ Nunez <jjbnunez97@gmail.com>
2024-02-19 15:02:22 -0500

    Correct installdir paths to C drive

--- il21946/CopyTo.ps1
+++ il21946/CopyTo.ps1
@@ -1,5 +1,5 @@
 $LocalDir = ".\structure"
-$InstallDir = "D:\Steam Games\steamapps\common\IL 2 Sturmovik 1946"
+$InstallDir = "C:\Program Files (x86)\Steam\steamapps\common\IL 2 Sturmovik 1946"
 
 # Main config file
 Copy-Item (Join-Path -Path $LocalDir -ChildPath "conf.ini") (Join-Path -Path $InstallDir -ChildPath "conf.ini")
