68ff697017e579dc4480c784c01addb551a94a80
JJ Nunez <jjbnunez97@gmail.com>
2024-12-31 18:20:45 -0500

    Change installdir after installing a second drive

--- il2gb/common.ps1
+++ il2gb/common.ps1
@@ -1,4 +1,4 @@
-$InstallDir = "C:\Program Files (x86)\Steam\steamapps\common\IL-2 Sturmovik Battle of Stalingrad"
+$InstallDir = "D:\SteamLibrary\steamapps\common\IL-2 Sturmovik Battle of Stalingrad"
 $DataDir = Join-Path $InstallDir "\data"
 $BinDir = Join-Path $InstallDir "\bin"
 $GameDir = Join-Path $BinDir "\game"
